63bc853dc14696200a9e2fbc0cc75d42be103492
selef <selef@192.168.0.4>
2019-03-21 14:44:57 -0300

    Version 1.1.ooo

--- src/java/aplicacion/controlador/Controlador.java
+++ src/java/aplicacion/controlador/Controlador.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+//Prueba 3
 package aplicacion.controlador;
 
 import aplicacion.modelo.comandos.FactoriaDeComandos;
